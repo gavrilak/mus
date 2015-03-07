3636d68cdf0492722c64cba9c3b0e4094c12e793
gavrilak <dima@dimas-MacBook-Pro.local>
2015-03-07 14:59:45 +0200

    Initial Commit

---
+++ music/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  music
+//
+//  Created by dima on 3/7/15.
+//  Copyright (c) 2015 dima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ music/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  music
+//
+//  Created by dima on 3/7/15.
+//  Copyright (c) 2015 dima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
